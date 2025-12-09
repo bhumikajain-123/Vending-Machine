19beae0c81d639a6d4523c257732d673beeb9c10
bhumikajain-123 <bhumikajainjain08@gmail.com>
2025-12-09 17:09:07 +0530

    same above
    same above

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -14,6 +14,7 @@ class CategoryController extends Controller
     }
 
     public function view_products(){
+          $categories = AdminCategory::all();
         return view('admin.view_product',compact('categories'));
     }
 
